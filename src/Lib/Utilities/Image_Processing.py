ee1097e8e06e2cf9490b6dd1e986d74a94c78bd9
Roman Parak <Roman.Parak@outlook.com>
2023-04-14 10:48:39 +0200

    improvements of the repo. (opencv - bounding box)

--- src/Lib/Utilities/Image_Processing.py
+++ src/Lib/Utilities/Image_Processing.py
@@ -6,21 +6,94 @@ import cv2
 #   ../Lib/Utilities/General
 import Lib.Utilities.General
 
-"""
-XML = {'x_min': 950, 'y_min': 614, 'x_max': 1132, 'y_max': 752}
-YOLO = {'x_c': 0.504347, 'y_c': 0.442419, 'width': 0.087728, 'height': 0.089276}
+def Draw_Bounding_Box(image, bounding_box_properties, format, Color, fill_box, show_info):
+    """
+    Description:
+        ...
 
-res = Lib.Utilities.General.Convert_Annotation('YOLO', 'PASCAL_VOC', YOLO, {'x': width, 'y': height})
-print(res)
-"""
-def Draw_Bounding_Box(image, bounding_box_properties = {'Name': 'Obj_Name_Id_0', 'Accuracy': '100', 'Data': None}, format = 'YOLO/Pascal_VOC', 
-                      Color = (0, 255, 0), fill_box = False, visibility_info = False):
+    Example:
+        image_out = Draw_Bounding_Box(image, bounding_box_properties = {'Name': 'Obj_Name_Id_0', 'Accuracy': '100', 'Data': None}, format = 'YOLO/Pascal_VOC', 
+                                      Color = (0, 255, 0), fill_box = False, show_info = False)
+    """
+
+    image_out = image.copy()
+
+    # Set the properties of the drawing bounding box.
+    #   Resolution: [x: Height, y: Width]
+    Resolution = {'x': image_out.shape[0], 'y': image_out.shape[1]}
+    #   Line width of the rectangle.
+    line_width = 1
+    # Offset of an additional rectangles.
+    offset = 5
     
+ 
+    # Obtain data in PASCAL_VOC format to determine the bounding box to be rendered.
+    #   data = {'x_min', 'y_min', 'x_max', 'y_max'}
     if format == 'YOLO':
-        data = Lib.Utilities.General.Convert_Annotation(format, 'PASCAL_VOC', bounding_box_properties['Data']) 
+        data = Lib.Utilities.General.Convert_Annotation(format, 'PASCAL_VOC', bounding_box_properties['Data'], Resolution) 
     data = bounding_box_properties['Data']
 
-    image_copy = image.copy()
+    x_min = data['x_min']; y_min = data['y_min']
+    x_max = data['x_max']; y_max = data['y_max']
+    box_w = x_max - x_min; box_h = y_max - y_min
 
+    if fill_box == True:
+        # Transparency coefficient.
+        alpha = 0.10
 
-    # return image_out
+        # The main rectangle that bounds the object.
+        cv2.rectangle(image_out, (x_min, y_min), (x_max, y_max), Color, -1)
+
+        if show_info == True:
+            # Additional rectangles that bounds information about the object.
+            cv2.rectangle(image_out, (x_min, y_min - (int(box_h/4.0) + offset)), (x_min + box_w, y_min - offset), 
+                          Color, -1)
+            cv2.rectangle(image_out, (x_max + offset, y_min - (int(box_h/4.0) + offset)), (x_max + offset + int(box_w/2.0), y_min - offset), 
+                          Color, -1)
+            
+        image_out = cv2.addWeighted(image_out, alpha, image, 1 - alpha, 0)
+
+    # Draw a modified rectangle around the object.
+    #  --          --
+    # |              |    
+    #      Object    
+    # |              |
+    #  --          --
+    #   Corner: Left Top
+    cv2.line(image_out, (x_min, y_min), (x_min + int(box_w/4.0), y_min), Color, line_width)
+    cv2.line(image_out, (x_min, y_min), (x_min, y_min + int(box_h/4.0)), Color, line_width)
+    #   Corner: Left Bottom
+    cv2.line(image_out, (x_min, y_max), (x_min + int(box_w/4.0), y_max), Color, line_width)
+    cv2.line(image_out, (x_min, y_max), (x_min, y_max - int(box_h/4.0)), Color, line_width)
+    #   Corner: Right Top
+    cv2.line(image_out, (x_max, y_min), (x_max - int(box_w/4.0), y_min), Color, line_width)
+    cv2.line(image_out, (x_max, y_min), (x_max, y_min + int(box_h/4.0)), Color, line_width)
+    #   Corner: Right Bottom
+    cv2.line(image_out, (x_max, y_max), (x_max - int(box_w/4.0), y_max), Color, line_width)
+    cv2.line(image_out, (x_max, y_max), (x_max, y_max - int(box_h/4.0)), Color, line_width)
+
+    if show_info == True:
+         # The font of the text shown in the image.
+        txt_font = cv2.FONT_HERSHEY_SIMPLEX
+
+        # A rectangle with the name of the object.
+        cv2.rectangle(image_out, (x_min, y_min - (int(box_h/4.0) + offset)), (x_min + box_w, y_min - offset), 
+                      Color, line_width)
+        #   Get the text boundary with the object name.
+        #       Parameters: [0.5: font_scale, 1: thickness]
+        txt_name_boundary = cv2.getTextSize(bounding_box_properties['Name'], txt_font, 0.5, line_width)[0]
+
+        # Get the coefficient of the displacement difference between the rectangles.
+        #   Rectangle Id: Name
+        f = np.array([box_w/2.0, int(box_h/4.0)/2]) - np.array([txt_name_boundary[0]/2, txt_name_boundary[1]/2])
+        cv2.putText(image_out, bounding_box_properties['Name'], (x_min + int(f[0]), (y_min - offset) - int(f[1])), txt_font, 0.5, Color, line_width)
+
+        # A rectangle indicating the accuracy of the match.
+        cv2.rectangle(image_out, (x_max + offset, y_min - (int(box_h/4.0) + offset)), (x_max + offset + int(box_w/2.0), y_min - offset), 
+                      Color, line_width)
+        # For accuracy, we use the same method as for the name.
+        txt_name_boundary = cv2.getTextSize(bounding_box_properties['Accuracy'] + ' %', txt_font, 0.5, line_width)[0]
+        f = np.array([int(box_w/2.0)/2.0, int(box_h/4.0)/2]) - np.array([txt_name_boundary[0]/2, txt_name_boundary[1]/2])
+        cv2.putText(image_out, bounding_box_properties['Accuracy'] + ' %', (x_max + offset + int(f[0]), (y_min - offset) - int(f[1])), txt_font, 0.5, Color, line_width)
+
+    return image_out
--- src/Test_OpenCV/test.py
+++ src/Test_OpenCV/test.py
@@ -9,89 +9,15 @@ import cv2
 #   ../Lib/Utilities/Image_Processing
 import Lib.Utilities.Image_Processing
 
-# https://christianbernecker.medium.com/convert-bounding-boxes-from-coco-to-pascal-voc-to-yolo-and-back-660dc6178742
-
-
-def pascal_voc_to_yolo(x1, y1, x2, y2, image_w, image_h):
-    return [((x2 + x1)/(2*image_w)), ((y2 + y1)/(2*image_h)), (x2 - x1)/image_w, (y2 - y1)/image_h]
-
-def yolo_to_pascal_voc(x_center, y_center, w, h,  image_w, image_h):
-    w = w * image_w
-    h = h * image_h
-    x1 = ((2 * x_center * image_w) - w)/2
-    y1 = ((2 * y_center * image_h) - h)/2
-    x2 = x1 + w
-    y2 = y1 + h
-    return [x1, y1, x2, y2]
-
-def MyRec(rgb,x,y,w,h,v=20,color=(200,0,0),thikness =1):
-    """To draw stylish rectangle around the objects"""
-    cv2.line(rgb, (x,y),(x+v,y), color, thikness)
-    cv2.line(rgb, (x,y),(x,y+v), color, thikness)
-
-    cv2.line(rgb, (x+w,y),(x+w-v,y), color, thikness)
-    cv2.line(rgb, (x+w,y),(x+w,y+v), color, thikness)
-
-    cv2.line(rgb, (x,y+h),(x,y+h-v), color, thikness)
-    cv2.line(rgb, (x,y+h),(x+v,y+h), color, thikness)
-
-    cv2.line(rgb, (x+w,y+h),(x+w,y+h-v), color, thikness)
-    cv2.line(rgb, (x+w,y+h),(x+w-v,y+h), color, thikness)
-
 def main():
-    img = cv2.imread('Test_Image_10.png')
-    overlay = img.copy()
+    img_in = cv2.imread('Test_Image_10.png')
 
-    RGB_Color = (0,255,0)
+    BBox_Properties = {'Name': 'Obj_Name_Id_0', 'Accuracy': '100', 'Data': {'x_min': 950, 'y_min': 614, 'x_max': 1132, 'y_max': 752}}
+    img_out = Lib.Utilities.Image_Processing.Draw_Bounding_Box(img_in, BBox_Properties, 'PASCAL_VOC', (0, 255, 0), True, True)
 
-    height = overlay.shape[0]; width = overlay.shape[1]
-
-    XML = {'x_min': 950, 'y_min': 614, 'x_max': 1132, 'y_max': 752}
-
-    #print(pascal_voc_to_yolo(XML['x_min'], XML['y_min'], XML['x_max'], XML['y_max'], width, height))
-    #print(yolo_to_pascal_voc(YOLO['x_c'], YOLO['y_c'], YOLO['width'], YOLO['heigt'], width, height))
-
-    x, y, w, h = XML['x_min'], XML['y_min'], (XML['x_max'] - XML['x_min']), (XML['y_max'] - XML['y_min'])
-
-    cv2.rectangle(overlay,(x,y-35),(x+w,y-5),RGB_Color, -1)
-    cv2.rectangle(overlay,(XML['x_max'] + 5,y-35),(XML['x_max'] + 5 + 80,y-5),RGB_Color, -1)
-    cv2.rectangle(overlay,(x,y),(x+w,y+h),RGB_Color, -1)
-
-    alpha = 0.15  # Transparency factor.
-    # Following line overlays transparent rectangle over the image
-    image_new = cv2.addWeighted(overlay, alpha, img, 1 - alpha, 0)
-
-    # setup text
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    text = "T_Joint"
-    # get boundary of this text
-    textsize = cv2.getTextSize(text, font, 0.5, 1)[0]
-    text_c = np.array([textsize[0]/2, textsize[1]/2])
-    rect_c = np.array([w/2.0, 30/2])
-    f = rect_c - text_c
-    # ...
-    cv2.putText(image_new, text, (x + int(f[0]), (y - 5) - int(f[1])), font, 0.5, RGB_Color, 1)
-
-    # setup text
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    text = "89.99%"
-    # get boundary of this text
-    textsize = cv2.getTextSize(text, font, 0.5, 1)[0]
-    text_c = np.array([textsize[0]/2, textsize[1]/2])
-    rect_c = np.array([80.0/2.0, 30/2])
-    f = rect_c - text_c
-    # ...
-    cv2.putText(image_new, text, (XML['x_max'] + 5 + int(f[0]), (y - 5) - int(f[1])), font, 0.5, RGB_Color, 1)
-
-    MyRec(image_new, x, y, w, h, 20, RGB_Color)
-    cv2.rectangle(image_new,(x,y-35),(x+w,y-5),RGB_Color, 1)
-    cv2.rectangle(image_new,(XML['x_max'] + 5,y-35),(XML['x_max'] + 5 + 80,y-5),RGB_Color, 1)
-
-    """
-    cv2.imshow('Test OpenCV', image_new)
+    cv2.imshow('Test OpenCV', img_out)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    """
 
 if __name__ == '__main__':
     sys.exit(main())
