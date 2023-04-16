473476e76e7b1c99f79966df4a416fda5681e850
Roman Parak <Roman.Parak@outlook.com>
2023-04-16 14:46:23 +0200

    improvements of the repo.

--- src/Blender/gen_synthetic_data.py
+++ src/Blender/gen_synthetic_data.py
@@ -3,6 +3,8 @@ import sys
 #   Add access if it is not in the system path.
 if '../' + 'src' not in sys.path:
     sys.path.append('../' + 'src')
+# Numpy (Array computing) [pip3 install numpy]
+import numpy as np
 # Custom Library:
 #   ../Lib/Blender/Core & Utilities
 import Lib.Blender.Core
@@ -69,28 +71,40 @@ def main():
     # Enable (turn on) visibility of the object.
     Object_Cls.Visibility(True)
     
-    # Generates data up to the desired maximum number of iterations, which is given by the constant {CONST_NUM_OF_GEN_DATA}.
-    i = 0; id_partition = 0; percentage_stored_data = 0
-    while CONST_NUM_OF_GEN_DATA > i:
-        # Generate a random position of the object.
-        Object_Cls.Random()
+    try:
+        assert np.sum(list(CONST_PARTITION_DATASET.values())) == 100
 
-        # Get the 2D coordinates of the bounding box from the rendered object scanned by the camera.
-        bounding_box_2d = Lib.Utilities.General.Get_2D_Coordinates_Bounding_Box(Object_Cls.Vertices, Camera_Cls.P(), 
-                                                                                Lib.Parameters.Camera.PhoXi_Scanner_M_Str.Resolution, 'YOLO')
-        
-        # Calculate the current percentage of stored data.
-        if percentage_stored_data == (CONST_NUM_OF_GEN_DATA * (list(CONST_PARTITION_DATASET.values())[id_partition]/100)):
-            id_partition += 1; percentage_stored_data = 0
+        # Generates data up to the desired maximum number of iterations, which is given by the constant {CONST_NUM_OF_GEN_DATA}.
+        i = 0; id_partition = 0; percentage_stored_data = 0
+        while CONST_NUM_OF_GEN_DATA > i:
+            # Generate a random position of the object.
+            Object_Cls.Random()
 
-        # Get the name of the partition where the data will be stored.
-        partition_name = list(CONST_PARTITION_DATASET.keys())[id_partition]
+            # Get the 2D coordinates of the bounding box from the rendered object scanned by the camera.
+            bounding_box_2d = Lib.Utilities.General.Get_2D_Coordinates_Bounding_Box(Object_Cls.Vertices, Camera_Cls.P(), 
+                                                                                    Lib.Parameters.Camera.PhoXi_Scanner_M_Str.Resolution, 'YOLO')
+            
+            # Calculate the current percentage of stored data.
+            if percentage_stored_data == (CONST_NUM_OF_GEN_DATA * (list(CONST_PARTITION_DATASET.values())[id_partition]/100)):
+                id_partition += 1; percentage_stored_data = 0
 
-        # Save the image with the corresponding label.
-        Lib.Blender.Utilities.Save_Synthetic_Data(f'../Data/{partition_name}', f'{CONST_INIT_INDEX + (i + 1):05}', Object_Str.Id, list(bounding_box_2d.values()), 
-                                                  'txt', 'png')
-        i += 1; percentage_stored_data += 1
+                if list(CONST_PARTITION_DATASET.values())[id_partition] == 0:
+                    id_partition += 1
 
+            # Get the name of the partition where the data will be stored.
+            partition_name = list(CONST_PARTITION_DATASET.keys())[id_partition]
+
+            # Save the image with the corresponding label.
+            Lib.Blender.Utilities.Save_Synthetic_Data(f'../Data/{partition_name}', f'{CONST_INIT_INDEX + (i + 1):05}', Object_Str.Id, list(bounding_box_2d.values()), 
+                                                    'txt', 'png')
+            i += 1; percentage_stored_data += 1
+            
+    except AssertionError as error:
+        print(f'[ERROR] Information: {error}')
+        print('[ERROR] The sum of the partition dataset must be 100.')
+
+    print('[INFO] The data generation has been successfully completed.')
+    
     # Disable (turn off) visibility of the object.
     Object_Cls.Visibility(False)
 
