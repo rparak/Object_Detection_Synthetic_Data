8973006b8976b2bde6e3251b4e6e1e5e09f9bbbd
Roman Parak <Roman.Parak@outlook.com>
2023-04-20 13:43:47 +0200

    python script to save image from PhoxiControl

---
+++ src/Lib/Photoneo/save_image.py
@@ -0,0 +1,15 @@
+# System (Default)
+import sys
+#   Add access if it is not in the system path.
+if '../' + 'src' not in sys.path:
+    sys.path.append('../' + 'src')
+
+def main():
+    """
+    Description:
+        text ....
+    """
+    pass
+
+if __name__ == '__main__':
+    main()
