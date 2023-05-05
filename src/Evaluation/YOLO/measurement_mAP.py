24e55779528c70ccb98a6ef1b2d9540a95e7e2b0
Roman Parak <Roman.Parak@outlook.com>
2023-05-05 08:43:42 +0200

    improvements of the repo.

--- src/Evaluation/YOLO/measurement_mAP.py
+++ src/Evaluation/YOLO/measurement_mAP.py
@@ -3,22 +3,29 @@ import sys
 #   Add access if it is not in the system path.
 if '../' + 'src' not in sys.path:
     sys.path.append('../..')
-# ...
+# Matplotlib (Visualization) [pip3 install matplotlib]
+import matplotlib.pyplot as plt
+# OS (Operating system interfaces)
+import os
+# SciencePlots (Matplotlib styles for scientific plotting) [pip3 install SciencePlots]
+import scienceplots
+# Time (Time access and conversions)
+import time
+# PyTorch (Tensors and Dynamic neural networks) [pip3 install torch]
 import torch
+# Torchvision (Image and video datasets and models) [pip3 install torchvision]
 import torchvision.ops.boxes
 # Custom Library:
 #   ../Lib/Utilities/General
 import Lib.Utilities.General
- 
 
-# need to convert the yolo bbox to pascal_voc bbox!
-res = Lib.Utilities.General.Convert_Boundig_Box_Data('YOLO', 'PASCAL_VOC', {'x_c': 0.507994, 'y_c': 0.473122, 'width': 0.082849, 'height': 0.083549}, {'x': 2064, 'y': 1544})
-print(res)
+def main():
+    # Locate the path to the project folder
+    project_folder = os.getcwd().split('Blender_Synthetic_Data')[0] + 'Blender_Synthetic_Data'
 
-# Bounding box coordinates.
-ground_truth_bbox = torch.tensor([[962, 666, 1133, 795]], dtype=torch.float)
-prediction_bbox = torch.tensor([[964, 667, 1150, 790]], dtype=torch.float)
- 
-# Get iou.
-iou = torchvision.ops.boxes.box_iou(ground_truth_bbox, prediction_bbox)
-print('IOU : ', iou.numpy()[0][0])
+    # Mean Average Precision (mAP)
+    # x - Image Idenfication Number (ID)
+    # y - Score
+
+if __name__ == '__main__':
+    sys.exit(main())
